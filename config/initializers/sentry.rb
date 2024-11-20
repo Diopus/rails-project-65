5a95d2d56f3f8ca79fbfcd1bf875d344f7868270
diopus <googopus@gmail.com>
2024-11-20 20:51:35 +0100

    Fix config/initializers/sentry.rb

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,7 +1,17 @@
-# frozen_string_literal: true
-
 Sentry.init do |config|
-  config.breadcrumbs_logger = [:active_support_logger]
-  config.dsn = ENV['SENTRY_DSN']
-  config.enable_tracing = true
+  config.dsn = 'https://8ac9995921207bf9d5b5e0baef03a422@o4506713064407040.ingest.us.sentry.io/4508332296765440'
+  config.breadcrumbs_logger = %i[active_support_logger http_logger]
+
+  # Set traces_sample_rate to 1.0 to capture 100%
+  # of transactions for tracing.
+  # We recommend adjusting this value in production.
+  config.traces_sample_rate = 1.0
+  # or
+  # config.traces_sampler = lambda do |context|
+  #   true
+  # end
+  # Set profiles_sample_rate to profile 100%
+  # of sampled transactions.
+  # We recommend adjusting this value in production.
+  config.profiles_sample_rate = 1.0
 end
