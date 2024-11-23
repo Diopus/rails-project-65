6574942fa38a93cf29f3441e4975e8ba6e428c46
diopus <googopus@gmail.com>
2024-11-23 20:50:51 +0100

    Fix linters

--- app/controllers/web/auth_controller.rb
+++ app/controllers/web/auth_controller.rb
@@ -1,8 +1,9 @@
+# frozen_string_literal: true
+
 class Web::AuthController < ApplicationController
   def callback
     # auth_data = request.env['omniauth.auth']
   end
 
-  def request
-  end
+  def request; end
 end
