b6e62119546843c1bbabd203550b58386251d643
diopus <googopus@gmail.com>
2024-11-24 12:57:30 +0100

    Fix controllers/web hierarchy

--- app/controllers/web/auth_controller.rb
+++ app/controllers/web/auth_controller.rb
@@ -1,9 +1,28 @@
 # frozen_string_literal: true
 
-class Web::AuthController < ApplicationController
-  def callback
-    # auth_data = request.env['omniauth.auth']
-  end
+module Web
+  class AuthController < ApplicationController
+    def request
+      Rails.logger.info 'Entering Web::AuthController#request'
+    end
 
-  def request; end
+    def callback
+      Rails.logger.info('Callback reached')
+      render plain: 'Callback successful', status: :ok
+      auth = request.env['omniauth.auth']
+
+      user = User.find_or_create_by(provider: auth['provider'], uid: auth['uid']) do |u|
+        u.email = auth['info']['email']
+        u.name = auth['info']['name']
+        u.password = SecureRandom.hex(16)
+      end
+
+      if user.persisted?
+        session[:user_id] = user.id
+        redirect_to root_path, notice: 'Вы успешно вошли в систему.'
+      else
+        redirect_to root_path, alert: 'Не удалось войти в систему.'
+      end
+    end
+  end
 end
