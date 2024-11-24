6703504f45f93bfad8bbf54886e11a6baf05a2f0
diopus <googopus@gmail.com>
2024-11-24 17:35:13 +0100

    Remove action "request"
    Because of this action tests were failing

--- app/controllers/web/auth_controller.rb
+++ app/controllers/web/auth_controller.rb
@@ -2,27 +2,14 @@
 
 module Web
   class AuthController < ApplicationController
-    def request
-      Rails.logger.info 'Entering Web::AuthController#request'
-    end
-
     def callback
-      Rails.logger.info('Callback reached')
-      render plain: 'Callback successful', status: :ok
-      auth = request.env['omniauth.auth']
-
-      user = User.find_or_create_by(provider: auth['provider'], uid: auth['uid']) do |u|
-        u.email = auth['info']['email']
-        u.name = auth['info']['name']
-        u.password = SecureRandom.hex(16)
+      auth_hash = request.env['omniauth.auth']
+      user = User.find_or_create_by!(email: auth_hash['info']['email']) do |u|
+        u.name = auth_hash['info']['name']
       end
 
-      if user.persisted?
-        session[:user_id] = user.id
-        redirect_to root_path, notice: 'Вы успешно вошли в систему.'
-      else
-        redirect_to root_path, alert: 'Не удалось войти в систему.'
-      end
+      session[:user_id] = user.id
+      redirect_to root_path
     end
   end
 end
