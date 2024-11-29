e241592ecf4e3a901e576154fbb05a2b392ac433
diopus <googopus@gmail.com>
2024-11-29 20:54:53 +0100

    Add seeds for category, user and bulletin

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,32 @@
 # frozen_string_literal: true
 
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+images = ['key.jpg', 'door.jpg', 'puddle.jpg']
+
+%w[Клавиши Двери Лужи].each do |category_name|
+  Category.find_or_create_by!(name: category_name)
+end
+
+# Users
+User.find_or_create_by(email: 'pochta@pochta.com') do |user|
+  user.password = '123456'
+end
+
+5.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password
+  )
+end
+
+# Posts
+users = User.all
+users.each do |user|
+  rand(0..2).times do
+    user.bulletin.create!(
+      category: Category.all.sample,
+      description: Faker::Lorem.paragraph_by_chars(number: rand(Post.description_max_length)),
+      image: images.sample,
+      title: Faker::Lorem.paragraph_by_chars(number: rand(1..Post.title_max_length))
+    )
+  end
+end
