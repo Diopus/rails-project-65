7f2d72775aa6ed0f9316457adade6caae0c1cd00
diopus <googopus@gmail.com>
2024-11-29 19:45:31 +0100

    Add validations to app/models/bulletin.rb

--- app/models/bulletin.rb
+++ app/models/bulletin.rb
@@ -1,5 +1,20 @@
 class Bulletin < ApplicationRecord
+  MAX_TITLE_LENGTH = 50
+  MAX_DESCRIPTION_LENGTH = 1000
+
   belongs_to :category
   belongs_to :user
   has_one_attached :image
+
+  validates :title, presence: true, length: { maximum: MAX_TITLE_LENGTH }
+  validates :description, presence: true, length: { maximum: MAX_DESCRIPTION_LENGTH }
+  validates :image, attached: true, size: { less_than: 5.megabytes }
+
+  def self.title_max_length
+    MAX_TITLE_LENGTH
+  end
+
+  def self.description_max_length
+    MAX_DESCRIPTION_LENGTH
+  end
 end
