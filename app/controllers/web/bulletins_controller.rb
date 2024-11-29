b94874e2df40cf7c9c1cf1514d329478ff93fb79
diopus <googopus@gmail.com>
2024-11-29 20:09:23 +0100

    Add basic logic to web/bulletins_controller.rb

--- app/controllers/web/bulletins_controller.rb
+++ app/controllers/web/bulletins_controller.rb
@@ -1,4 +1,23 @@
 module Web
   class BulletinsController < ApplicationController
+    before_action :set_bulletin, only: %i[show edit update destroy]
+
+    def index
+      @bulletins = Bulletin.all
+    end
+
+    def show
+      @bulletin = Bulletin.find(params[:id])
+    end
+
+    private
+
+    def bulletin_params
+      params.require(:bulletin).permit(:title, :description, :image)
+    end
+
+    def set_bulletin
+      @bulletin = Bulletin.find params[:id]
+    end
   end
 end
