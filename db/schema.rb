ea78cfd6651c59efc82bdaa8031908f8a871c2ac
diopus <googopus@gmail.com>
2024-11-29 19:26:06 +0100

    Apply migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_28_175752) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_29_181520) do
+  create_table "bulletins", force: :cascade do |t|
+    t.string "title", limit: 50, null: false
+    t.text "description", limit: 1000, null: false
+    t.string "image", null: false
+    t.integer "category_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_bulletins_on_category_id"
+    t.index ["user_id"], name: "index_bulletins_on_user_id"
+  end
+
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
@@ -23,4 +35,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_28_175752) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "bulletins", "categories"
+  add_foreign_key "bulletins", "users"
 end
