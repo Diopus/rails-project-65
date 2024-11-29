9522e9e7f29615759fb9db1945ca18087e913752
diopus <googopus@gmail.com>
2024-11-29 19:22:17 +0100

    Add "null: false" and limit conditions to db/migrate/20241129181520_create_bulletins.rb

--- db/migrate/20241129181520_create_bulletins.rb
+++ db/migrate/20241129181520_create_bulletins.rb
@@ -1,9 +1,9 @@
 class CreateBulletins < ActiveRecord::Migration[7.2]
   def change
     create_table :bulletins do |t|
-      t.string :title
-      t.text :description
-      t.string :image
+      t.string :title, null: false, limit: 50
+      t.text :description, null: false, limit: 1000
+      t.string :image, null: false
       t.references :category, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
 
