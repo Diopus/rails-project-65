d2ebcdcd9e707056bdc95d5546a1991eb9f9d386
diopus <googopus@gmail.com>
2024-11-29 18:47:06 +0100

    Apply migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_23_154819) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_28_175752) do
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
