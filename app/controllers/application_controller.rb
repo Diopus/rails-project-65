419105cea2bb8155390c5258c634dbef16511207
diopus <googopus@gmail.com>
2024-11-30 16:04:48 +0100

    Add helper methods to app/controllers/application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  include Authentication
+
+  helper_method :user_signed_in, :current_user
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 end
