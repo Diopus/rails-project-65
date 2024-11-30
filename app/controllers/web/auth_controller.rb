ba9539db508140928d4c9ae381001b3779083638
diopus <googopus@gmail.com>
2024-11-30 15:25:34 +0100

    Add "find_or_create_user" private method to auth_controller.rb

--- app/controllers/web/auth_controller.rb
+++ app/controllers/web/auth_controller.rb
@@ -4,12 +4,18 @@ module Web
   class AuthController < ApplicationController
     def callback
       auth_hash = request.env['omniauth.auth']
-      user = User.find_or_create_by!(email: auth_hash['info']['email']) do |u|
-        u.name = auth_hash['info']['name']
-      end
+      user = find_or_create_user(auth_hash)
 
       sign_in(user)
       redirect_to root_path
+    end
+
+    private
+
+    def find_or_create_user(auth_hash)
+      User.find_or_create_by!(email: auth_hash['info']['email']) do |u|
+        u.name = auth_hash['info']['name']
+      end
     end
   end
 end
