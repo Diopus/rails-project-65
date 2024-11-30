e0dbeed31b367180bdb015fa75b4f2fe89452c33
diopus <googopus@gmail.com>
2024-11-30 15:17:51 +0100

    Fix parent class for Web::BulletinsController

--- app/controllers/web/bulletins_controller.rb
+++ app/controllers/web/bulletins_controller.rb
@@ -1,35 +1,33 @@
 # frozen_string_literal: true
 
-module Web
-  class BulletinsController < ApplicationController
-    before_action :set_bulletin, only: %i[show edit update destroy]
+class Web::BulletinsController < ApplicationController
+  before_action :set_bulletin, only: %i[show edit update destroy]
 
-    def index
-      @bulletins = Bulletin.all
-    end
+  def index
+    @bulletins = Bulletin.all
+  end
 
-    def show
-      @bulletin = Bulletin.find(params[:id])
-    end
+  def show
+    @bulletin = Bulletin.find(params[:id])
+  end
 
-    def new; end
+  def new; end
 
-    def edit; end
+  def edit; end
 
-    def create; end
+  def create; end
 
-    def update; end
+  def update; end
 
-    def destroy; end
+  def destroy; end
 
-    private
+  private
 
-    def bulletin_params
-      params.require(:bulletin).permit(:title, :description, :image)
-    end
+  def bulletin_params
+    params.require(:bulletin).permit(:title, :description, :image)
+  end
 
-    def set_bulletin
-      @bulletin = Bulletin.find params[:id]
-    end
+  def set_bulletin
+    @bulletin = Bulletin.find params[:id]
   end
 end
