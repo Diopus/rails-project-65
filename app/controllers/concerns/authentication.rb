d318d0bb975bd2761142ecb97f6a4253fef0a066
diopus <googopus@gmail.com>
2024-11-30 15:34:49 +0100

    Add "current_user", "sign_out" and "user_signed_in?" methods to authentication.rb

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -1,7 +1,20 @@
 # frozen_string_literal: true
 
 module Authentication
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
   def sign_in(user)
     session[:user_id] = user.id
+  end
+
+  def sign_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  def user_signed_in?
+    session[:user_id].present? && current_user.present?
   end
 end
