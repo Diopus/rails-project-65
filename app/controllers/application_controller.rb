e0b393f50c9156ee67e05bd6afbe4f69bf3c55f1
diopus <googopus@gmail.com>
2024-11-30 17:07:51 +0100

    Fix helper name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@
 class ApplicationController < ActionController::Base
   include Authentication
 
-  helper_method :user_signed_in, :current_user
+  helper_method :user_signed_in?, :current_user
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
