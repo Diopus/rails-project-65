70b6591ecee7160a109651f0e977175cfb72888d
diopus <googopus@gmail.com>
2024-12-01 20:39:33 +0100

    Fix namespace for admin/bulletins_controller.rb

--- app/controllers/admin/bulletins_controller.rb
+++ app/controllers/admin/bulletins_controller.rb
@@ -1,20 +1,22 @@
 # frozen_string_literal: true
 
-class Web::BulletinsController < ApplicationController
-  # TODO: allowed actions
-  before_action :set_bulletin, only: %i[index]
+module Admin
+  class BulletinsController < ApplicationController
+    # TODO: allowed actions
+    before_action :set_bulletin, only: %i[index]
 
-  def index
-    @bulletins = Bulletin.all
-  end
+    def index
+      @bulletins = Bulletin.all
+    end
 
-  private
+    private
 
-  def bulletin_params
-    params.require(:bulletin).permit(:title, :description, :image)
-  end
+    def bulletin_params
+      params.require(:bulletin).permit(:title, :description, :image)
+    end
 
-  def set_bulletin
-    @bulletin = Bulletin.find params[:id]
+    def set_bulletin
+      @bulletin = Bulletin.find params[:id]
+    end
   end
 end
