9e04a0ee052bd140e3b0c450f81455e5b6fc3a76
diopus <googopus@gmail.com>
2024-12-01 16:14:19 +0100

    Move "auth" out of ":locale" scope

--- config/routes.rb
+++ config/routes.rb
@@ -13,11 +13,13 @@ Rails.application.routes.draw do
     scope module: :web do
       root 'bulletins#index'
 
-      post 'auth/:provider', to: 'auth#request', as: :auth_request
-      get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
-      delete 'auth/logout', to: 'auth#logout'
-
       resources :bulletins
     end
+  end
+
+  scope module: :web do
+    post 'auth/:provider', to: 'auth#request', as: :auth_request
+    get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
+    delete 'auth/logout', to: 'auth#logout'
   end
 end
