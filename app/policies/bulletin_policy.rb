f1b54ab9c9296b08fbd5ef308f95e5c3c8d1ef4c
diopus <googopus@gmail.com>
2025-01-09 11:26:30 +0100

    Add BulletinPolicy using pundit

---
+++ app/policies/bulletin_policy.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+class BulletinPolicy < ApplicationPolicy
+  attr_reader :user, :bulletin
+
+  def initialize(user, bulletin)
+    @user = user
+    @bulletin = bulletin
+  end
+
+  def show?
+    author? || admin?
+  end
+
+  def edit?
+    author?
+  end
+
+  def update?
+    edit?
+  end
+
+  def destroy?
+    admin?
+  end
+
+  def moderate?
+    author
+  end
+
+  private
+
+  def admin?
+    user&.admin?
+  end
+
+  def author?
+    user && bulletin.user == user
+  end
+end
