d08df43613cf4c4e9b80d1497622315b6092faa9
diopus <googopus@gmail.com>
2025-01-09 11:18:02 +0100

    Fix pundit NotAuthorizedError processing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,16 +8,23 @@ class ApplicationController < ActionController::Base
 
   helper_method :user_signed_in?, :current_user
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
   private
 
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
 
-  def default_url_options
-    { locale: I18n.locale }
+  def user_not_authorized
+    flash[:alert] = t('auth.not_authorized') # 'You are not authorized to perform this action.'
+    redirect_back_or_to(root_path)
   end
 end
