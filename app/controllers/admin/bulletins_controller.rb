8d5f922f11fd16640b25b4a365627cc69414b3cb
diopus <googopus@gmail.com>
2025-01-09 17:33:12 +0100

    Add aasm to admin/bulletins_controller

--- app/controllers/admin/bulletins_controller.rb
+++ app/controllers/admin/bulletins_controller.rb
@@ -2,14 +2,49 @@
 
 module Admin
   class BulletinsController < ApplicationController
+    before_action :set_bulletin, only: %i[publish reject]
+
     def index
       @bulletins = Bulletin.order('created_at desc')
+    end
+
+    # AASM
+    def publish
+      authorize @bulletin
+
+      if @bulletin.may_publish?
+        @bulletin.publish!
+
+        flash.now[:notice] = I18n.t('aasm.bulletin.transitions.publish.success')
+      else
+        flash.now[:alert] = I18n.t('aasm.bulletin.transitions.publish.failure')
+      end
+  
+      redirect_back fallback_location: bulletins_path
+    end
+  
+    def reject
+      authorize @bulletin
+
+      if @bulletin.may_reject?
+        @bulletin.reject!
+
+        flash.now[:notice] = I18n.t('aasm.bulletin.transitions.reject.success')
+      else
+        flash.now[:alert] = I18n.t('aasm.bulletin.transitions.reject.failure')
+      end
+  
+      redirect_back fallback_location: bulletins_path
     end
 
     private
 
     def bulletin_params
       params.require(:bulletin).permit(:title, :description, :image)
+    end
+
+    def set_bulletin
+      @bulletin = Bulletin.find params[:id]
     end
   end
 end
