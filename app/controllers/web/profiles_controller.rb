2dc6b608c687c7641603ba6526120130f0dd2fa0
diopus <googopus@gmail.com>
2025-01-09 17:34:23 +0100

    Add profiles_controller

---
+++ app/controllers/web/profiles_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Web
+  class ProfilesController < ApplicationController
+    before_action :authenticate_user!
+
+    def show
+      @bulletins = current_user.bulletins.order('created_at desc')
+    end
+  end
+end
