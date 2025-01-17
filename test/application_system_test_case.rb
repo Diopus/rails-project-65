75a5d975d8a01fb6ff3a84435d58d89db829af3a
diopus <googopus@gmail.com>
2025-01-17 16:05:28 +0100

    Add 'test_file' helper for system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -6,4 +6,8 @@ class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
 
   include AuthenticationHelpers
+
+  def test_file(filename)
+    Rails.root.join('test/fixtures/files', filename)
+  end
 end
